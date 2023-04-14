f701a1cc0a26f90a3da663c500805dd578f3bc8c
Alex Inkiläinen <alexinkilainen@gmail.com>
2023-04-14 15:35:48 +0300

    Hienosäätöä ja siistimistä
    Poistettu vantaan asemilta stationId:t koska ne ei ole käyttäjäystävääliset

--- src/location/locationHandler.js
+++ src/location/locationHandler.js
@@ -4,7 +4,7 @@ const utils = require('../utils/index')
 async function getCityBikeStations (userLocation) {
   const query = `
   {
-    nearest(lat: ${userLocation.latitude}, lon: ${userLocation.longitude}, maxDistance: 1500, maxResults: 4, filterByPlaceTypes: BICYCLE_RENT) {
+    nearest(lat: ${userLocation.latitude}, lon: ${userLocation.longitude}, maxDistance: 1500, maxResults: 5, filterByPlaceTypes: BICYCLE_RENT) {
       edges {
         node {
           distance
@@ -14,7 +14,9 @@ async function getCityBikeStations (userLocation) {
               name
               bikesAvailable
               spacesAvailable
+              capacity
               state
+              networks
             }
           }
         }
@@ -27,19 +29,32 @@ async function getCityBikeStations (userLocation) {
 }
 
 function stationListaus (response) {
-  let valmisViesti = '<b>Kaupunkipyöräasemat lähelläsi:</b>\n\n'
+  let valmisViesti = '<b>Kaupunkipyöräasemat lähelläsi</b>\n\n'
   const edges = response.nearest.edges
   // Jos asemia ei löydy käyttäjän läheltä
   if (response.nearest.edges.length === 0) {
-    console.info('Ei pyöräasemia käyttäjän lähellä')
-    return 'Ei pyöräasemia lähelläsi'
+    console.info('Ei asemia käyttäjän lähellä')
+    return 'Valitettavasti läheltäsi ei löydy asemia 😕'
   } else {
     // Käydään jokainen asema läpi
     for (let i = 0; i < edges.length; i++) {
-      if (edges[i].node.place.state === 'Station off') {
-        valmisViesti = valmisViesti + `<b>${edges[i].node.place.name}</b> ${edges[i].node.place.stationId} ➝ ${edges[i].node.distance}m\nAsema ei ole käytössä.\n\n`
+      console.log(edges[i].node.place.networks[0])
+      // Jos vantaan asemat otetaan stationId pois koska vantaan stationId:t = nonsense
+      if (edges[i].node.place.networks[0] === 'vantaa') {
+        // Tarkistaa onko asema käytössä ja rakentaa viestin
+        if (edges[i].node.place.state === 'Station off') {
+          valmisViesti = valmisViesti + `<b>${edges[i].node.place.name}</b> ➝ ${edges[i].node.distance}m\nAsema ei ole käytössä.\n\n`
+        } else {
+          valmisViesti = valmisViesti + `<b>${edges[i].node.place.name}</b> ➝ ${edges[i].node.distance}m\nPyöriä asemalla ${edges[i].node.place.bikesAvailable}/${edges[i].node.place.capacity}\n\n`
+        }
       } else {
-        valmisViesti = valmisViesti + `<b>${edges[i].node.place.name}</b> ${edges[i].node.place.stationId} ➝ ${edges[i].node.distance}m\nPyöriä asemalla: ${edges[i].node.place.bikesAvailable}\nPaikkoja vapaana: ${edges[i].node.place.spacesAvailable}\n\n`
+        // Helsinki & Espoo (smoove) tai muu
+        // Tarkistaa onko asema käytössä ja rakentaa viestin
+        if (edges[i].node.place.state === 'Station off') {
+          valmisViesti = valmisViesti + `<b>${edges[i].node.place.name}</b> ${edges[i].node.place.stationId} ➝ ${edges[i].node.distance}m\nAsema ei ole käytössä.\n\n`
+        } else {
+          valmisViesti = valmisViesti + `<b>${edges[i].node.place.name}</b> ${edges[i].node.place.stationId} ➝ ${edges[i].node.distance}m\nPyöriä asemalla ${edges[i].node.place.bikesAvailable}/${edges[i].node.place.capacity}\n\n`
+        }
       }
     }
   }
